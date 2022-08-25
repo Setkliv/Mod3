5c6989b1c84c08a0fb2c3e3d58ea8203efef4065
Setkliv <vovakosachev@mail.ru>
2022-08-25 17:12:50 +0300

    3

--- main.cs
+++ main.cs
@@ -5,13 +5,13 @@ class MainClass {
     
 		const string MyName = "Владимир";
 		byte MyAge = 23;
-		bool HaveIApet = нет;
+		bool HaveIApet = false;
 		double MyShoeSize = 40.0;
 
-		Console.WriteLine("Меня зовут" + MyName);
+		Console.WriteLine("Меня зовут " + MyName);
 		Console.WriteLine("Мне {0} года", MyAge);
 		Console.WriteLine("У меня {0} домашнего питомца", HaveIApet);
-		Console.WriteLine("У меня {0} размер", MyShoeSize);
+		Console.WriteLine("У меня {0} размер ноги", MyShoeSize);
 		Console.ReadKey();
 
   }
